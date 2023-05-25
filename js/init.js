b877e5de7596e7442a69e08948480b3347951fbe
Zack Pakin <zpakin@ucla.edu>
2023-05-25 15:16:07 -0700

    added new plugin

--- js/init.js
+++ js/init.js
@@ -1,12 +1,13 @@
 // declare variables
-let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+let mapOptions = { 'center': [34.0709, -118.444], 'zoom': 5 }
 
-let vaccinated = L.featureGroup();
-let nonVaccinated = L.featureGroup();
+
+let vaccinated = L.markerClusterGroup();
+let nonVaccinated = L.markerClusterGroup();
 
 let layers = {
-	"Vaccinated Respondent": vaccinated,
-	"Unvaccinated Respondent": nonVaccinated
+    "Vaccinated Respondent": vaccinated,
+    "Unvaccinated Respondent": nonVaccinated
 }
 
 let circleOptions = {
@@ -24,43 +25,43 @@ const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSNq8_prhrSwK3C
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
 
 // add layer control box
-L.control.layers(null,layers).addTo(map)
+L.control.layers(null, layers).addTo(map)
 
 let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+    maxZoom: 16
 });
 
 Esri_WorldGrayCanvas.addTo(map);
 
-function addMarker(data){
-    if(data['Have you been vaccinated?'] == "Yes"){
+function addMarker(data) {
+    if (data['Have you been vaccinated?'] == "Yes") {
         circleOptions.fillColor = "red"
-        vaccinated.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>Vaccinated</h2>`))
-        createButtons(data.lat,data.lng,data['Where did you get vaccinated?'])
-        }
-    else{
+        vaccinated.addLayer(L.circleMarker([data.lat, data.lng], circleOptions).bindPopup(`<h2>Vaccinated</h2>`))
+        createButtons(data.lat, data.lng, data['Where did you get vaccinated?'])
+    }
+    else {
         circleOptions.fillColor = "blue"
-        nonVaccinated.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>Non-Vaccinated</h2>`))
-        createButtons(data.lat,data.lng,data['Where did you get vaccinated?'])
+        nonVaccinated.addLayer(L.circleMarker([data.lat, data.lng], circleOptions).bindPopup(`<h2>Non-Vaccinated</h2>`))
+        createButtons(data.lat, data.lng, data['Where did you get vaccinated?'])
     }
     return data
 }
 
-function createButtons(lat,lng,title){
+function createButtons(lat, lng, title) {
     const newButton = document.createElement("button"); // adds a new button
-    newButton.id = "button"+title; // gives the button a unique id
+    newButton.id = "button" + title; // gives the button a unique id
     newButton.innerHTML = title; // gives the button a title
-    newButton.setAttribute("lat",lat); // sets the latitude 
-    newButton.setAttribute("lng",lng); // sets the longitude 
-    newButton.addEventListener('click', function(){
-        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+    newButton.setAttribute("lat", lat); // sets the latitude 
+    newButton.setAttribute("lng", lng); // sets the longitude 
+    newButton.addEventListener('click', function () {
+        map.flyTo([lat, lng]); //this is the flyTo from Leaflet
     })
     const spaceForButtons = document.getElementById('placeForButtons')
     spaceForButtons.appendChild(newButton);//this adds the button to our page.
 }
 
-function loadData(url){
+function loadData(url) {
     Papa.parse(url, {
         header: true,
         download: true,
@@ -68,7 +69,7 @@ function loadData(url){
     })
 }
 
-function processData(results){
+function processData(results) {
     console.log(results)
     results.data.forEach(data => {
         console.log(data)
@@ -76,7 +77,7 @@ function processData(results){
     })
     vaccinated.addTo(map) // add our layers after markers have been made
     nonVaccinated.addTo(map) // add our layers after markers have been made  
-    let allLayers = L.featureGroup([vaccinated,nonVaccinated]);
+    let allLayers = L.featureGroup([vaccinated, nonVaccinated]);
     map.fitBounds(allLayers.getBounds());
 }
 
